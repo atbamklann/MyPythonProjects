96d580d3e566e2d368ba03a503b77260efa98261
atbamklann <yerkinbazarov@gmail.com>
2026-07-03 22:26:40 +0000

    day 1

--- first_program.py
+++ first_program.py
@@ -1,2 +1,64 @@
-print('Привет!')
-print('Мир!')
+# print("Привет!")
+# print("Мир!")
+
+
+# print("Как тебя зовут?")
+
+# name = input()
+# print("Привет,", name)
+
+# name = "Timur"
+# print("Привет,", name)
+
+
+# name1 = "Тимур"
+# name2 = name1
+# name1 = "Гвидо"
+
+# print(name1)
+# print(name2)
+
+# first = input()
+# second = input()
+
+# print("I am", first, "and", second)
+
+# name_1 = input()  # принимаем имя первого человека
+# print("Привет,", name_1, "!")  # делаем первый вывод
+
+# name_2 = input()  # принимаем имя второго человека
+# print("Здравствуйте,", name_2, ".")  # делаем второй вывод
+
+# a = input()
+# b = input()
+# c = input()
+
+# print(a, b, c)
+
+# a = '10'
+# b = '20'
+# c = '30'
+
+# print(a b c)
+
+# print("YES", sep="!", end="#")
+# print("NO", sep="#", end="!")
+
+# 1 print('a', 'b', 'c', sep='*')
+# 2 print('d', 'e', 'f', sep='**', end='') print('g', 'h', 'i', sep='+', end='%') print('j', 'k', 'l', sep='-', end='\n')
+# 3 print('m', 'n', 'o', sep='/', end='!') print('p', 'q', 'r', sep='1', end='%') print('s', 't', 'u', sep='&', end='\n')
+# 4 print('v', 'w', 'x', sep='%')
+# 5 print('y', 'z', sep='/', end='!')
+
+
+# print("БЕСКОНЕЧНОСТЬ_НЕ_ПРЕДЕЛ", end="")
+# print("!")
+
+# name = input()
+# print("Привет,", name, end="!")
+
+# sep1 = "*"
+# a = "Раз"
+# b = "Два"
+# c = "Три"
+# print(a, b, c, sep=sep1, end="")
